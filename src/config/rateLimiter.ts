51f6ce5a0e060a59dfc2e11e65a8e3155bccb00e
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2025-02-03 22:15:07 +0530

    feat: Apply rateLimiter

--- src/config/rateLimiter.ts
+++ src/config/rateLimiter.ts
@@ -13,3 +13,5 @@ export const initRateLimiter = (mongooseConnection: Connection) => {
         duration: DURATION
     })
 }
+
+
--- src/service/validationService.ts
+++ src/service/validationService.ts
@@ -1,5 +1,6 @@
 import joi from 'joi'
 import {
+    IChangePasswordRequestBody,
     IForgotPasswordRequestBody,
     ILoginUserRequestBody,
     IRegisterUserRequestBody,
@@ -26,7 +27,11 @@ export const ValidateForgotPasswordBody = joi.object<IForgotPasswordRequestBody,
 export const ValidateResetPasswordBody = joi.object<IResetPasswordRequestBody, true>({
     newPassword: joi.string().min(8).max(24).trim().required()
 })
-
+export const ValidateChangePasswordBody = joi.object<IChangePasswordRequestBody, true>({
+    oldPassword: joi.string().min(8).max(24).trim().required(),
+    newPassword: joi.string().min(8).max(24).trim().required(),
+    confirmNewPassword: joi.string().min(8).max(24).trim().valid(joi.ref('newPassword')).required()
+})
 
 export const validateJoiSchema = <T>(schema: joi.Schema, value: unknown) => {
     const result = schema.validate(value)
